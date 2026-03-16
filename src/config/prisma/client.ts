2d2a8ab489468c16fe9e1f591d5ff0306790f017
sitsofecode <sitsofesenouvo@gmail.com>
2026-03-16 10:22:48 +0100

    update prisma

--- src/config/prisma/client.ts
+++ src/config/prisma/client.ts
@@ -1,9 +1,6 @@
 import { PrismaClient } from '@prisma/client';
-import { PrismaPg } from '@prisma/adapter-pg';
+import { withAccelerate } from '@prisma/extension-accelerate';
 
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL!,
-});
+const prisma = new PrismaClient().$extends(withAccelerate());
 
-const prisma = new PrismaClient({ adapter });
 export default prisma;
