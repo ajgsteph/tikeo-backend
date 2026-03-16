9ef92dbf14e984f78cea23fb5957890ee7694ded
sitsofecode <sitsofesenouvo@gmail.com>
2026-03-16 09:55:15 +0100

    update prisma

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,15 +1,9 @@
 import { defineConfig } from 'prisma/config';
-import { PrismaPg } from '@prisma/adapter-pg';
 
 export default defineConfig({
   earlyAccess: true,
   schema: 'prisma/schema.prisma',
   migrate: {
-    async adapter() {
-      const { PrismaPg } = await import('@prisma/adapter-pg');
-      return new PrismaPg({
-        connectionString: process.env.DATABASE_URL!,
-      });
-    },
+    url: process.env.DATABASE_URL!,
   },
 });
