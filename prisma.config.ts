597a734779bf6b7feb9a1fbbedb4af7a8352f382
sitsofecode <sitsofesenouvo@gmail.com>
2026-03-16 10:07:47 +0100

    update prisma

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'prisma/config';
 export default defineConfig({
   earlyAccess: true,
   schema: 'prisma/schema.prisma',
-  migrate: {
+  datasource: {
     url: process.env.DATABASE_URL!,
   },
 });
